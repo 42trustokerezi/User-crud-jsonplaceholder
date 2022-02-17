26f5cf3122b74f40bc657b336b67ab52b0f4c21b
Trust Okerezi <trustokerezi42@gmail.com>
2022-02-17 16:48:44 +0100

    Add post functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import AddPost from './components/AddPost';
 
 import Post from './components/Post'
 
@@ -19,12 +20,46 @@ function App() {
     })
   }
 
+  const onAdd  = async (title, body) => {
+    await fetch('http://jsonplaceholder.typicode.com/users/1/posts', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: title,
+        body: body
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      }
+    })
+    .then((res) => {
+      if(res.status != 201){
+        return
+      }else{
+        return res.json()
+      }
+    })
+    .then((data) => {
+      setPosts((posts) => [...posts, data]);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+
+  const onDelete = async (id) => {
+    await fetch(`http://jsonplaceholder.typicode.com/users/1/posts/${id}`, {
+      method: 'DELETE',
+    })
+  }
+
 
   console.log(posts);
 
   return (
     <div className="App">
       <h3>CRUD App using JsonPlaceholder</h3>
+      <br />
+      <AddPost onAdd={onAdd} />
       <div>
         {
           posts.map((post) => (
